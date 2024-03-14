c6785b8340351e5a17aa46e8b6e0222751bc06e4
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-14 21:05:46 +0900

    feat: 클라이언트가 회원가입 폼을 제출하면 원래 홈페이지로 리다이렉트하는 기능 구현
    htmlRequest가 기본 주소와 파일을 가지고 RegisterRequest에게 전달

--- src/main/java/webserver/HtmlRequest.java
+++ src/main/java/webserver/HtmlRequest.java
@@ -8,28 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class HtmlRequest {
+public class HtmlRequest implements Request{
     private static final Logger logger = LoggerFactory.getLogger(HtmlRequest.class);
     private static final String BASE_DIR = "src/main/resources/static";
     private final String url;
 
     // /index.html -> /static/index.html
     // /register.html 와 /registration/index.html -> static/registration/index.html
-    // htmlName 은 <index.html> , <register.html> , <registration.html> 중 하나
+    // url 은 index.html , register.html , registration.html 등등이 들어옴.
     public HtmlRequest(final String url) {
         this.url = url;
+    }
+
+    public HtmlRequest() {
+        this("");
     }
 
     public Response execute() throws IOException {
         // <index.html> 과 <register.html> 인지 찾기
         final DirectoryPath path = DirectoryPath.findFrom(url);
         final File file = decide(path);
+        final byte[] fileDatas = readFile(file);
 
-        final byte[] htmlFileDatas = new byte[(int) file.length()];
-        try (final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
-            bis.read(htmlFileDatas);
-        }
-        return new Response(htmlFileDatas);
+        return new Response(fileDatas);
+    }
+
+    public byte[] getBaseHtmlFile() throws IOException{
+        final File baseFile = new File(BASE_DIR + DirectoryPath.STATIC.fileName);
+        return readFile(baseFile);
     }
 
     // index.html 과 register.html 을 못찾으면 static 디렉토리를 한개씩 열어보며 URL 과 일치하는 .html 파일을 찾기
@@ -43,8 +49,9 @@ public class HtmlRequest {
 
     private File findHTMLFile() {
         final List<File> files = new ArrayList<>();
-        final File dir = new File(BASE_DIR);
-        searchHTMLFile(dir, files);
+        final File baseDir = new File(BASE_DIR);
+        // static 폴더 아래의 폴더 및 파일들을 재귀적으로 순회하면서 모든 .html 파일을 찾는다.
+        searchHTMLFile(baseDir, files);
 
         return files.stream()
                 .filter(file -> file.getPath().contains(url.replace("/", "\\")))
@@ -53,14 +60,22 @@ public class HtmlRequest {
     }
 
     private void searchHTMLFile(File dir, List<File> files) {
-        File[] underDir = dir.listFiles();
-        for (File file : underDir) {
-            if (file.isDirectory()) {
-                searchHTMLFile(file, files);
-            } else if (file.isFile() && file.getName().endsWith(".html")) {
-                files.add(file);
+        File[] subDirectorys = dir.listFiles();
+        for (File sub : subDirectorys) {
+            if (sub.isDirectory()) {
+                searchHTMLFile(sub, files);
+            } else if (sub.isFile() && sub.getName().endsWith(".html")) {
+                files.add(sub);
             }
         }
+    }
+
+    private byte[] readFile(final File file) throws IOException{
+        final byte[] htmlFileDatas = new byte[(int) file.length()];
+        try (final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+            bis.read(htmlFileDatas);
+        }
+        return htmlFileDatas;
     }
 
     private enum DirectoryPath {
