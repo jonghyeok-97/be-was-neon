d6fd07cb995cdab631ef3ec0fb889e12501e6456
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-22 16:06:26 +0900

    refactor: utils 클래스의 접근 제어자를 private로 변경

--- src/main/java/webserver/utils/CRLF.java
+++ src/main/java/webserver/utils/CRLF.java
@@ -3,6 +3,8 @@ package webserver.utils;
 public class CRLF {
     private static final String CRLF = "\r\n";
 
+    private CRLF(){}
+
     public static String addNewLine(final String line) {
         return line + CRLF;
     }
--- src/main/java/webserver/utils/FileReader.java
+++ src/main/java/webserver/utils/FileReader.java
@@ -4,10 +4,14 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class FileReader {
 
+    private FileReader(){}
+
     public static byte[] read(final File file) throws IOException {
+        Objects.requireNonNull(file, file + "을 못 읽습니다.");
         final byte[] fileDatas = new byte[(int) file.length()];
         try (final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
             bis.read(fileDatas);
