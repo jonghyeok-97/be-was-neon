a579b07a6ec2ff75a4657e07bbb3e6ba82d47843
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-28 21:03:48 +0900

    refactor: User의 생성방식을 빌더패턴에서 static 메소드로 변경
    생성자의 매개변수가 적기 때문에 빌더패턴 대신 static 메소드를 사용해서 유저의 생성 방식을 명시함

--- src/main/java/webserver/model/User.java
+++ src/main/java/webserver/model/User.java
@@ -13,31 +13,20 @@ public class User {
         this.name = name;
     }
 
-    // 유저 Id와 pw를 필수로 받게 하고 싶어서 builder를 사용했지만
-    // 팩토리메소드를 사용하는 선택이 더 좋았겠다.
-    // builder는 매개변수 많을 떄 사용하자!
-    static public class Builder {
-        private String userId;
-        private String password;
-        private String name;
+    public static User createUserForRegister(String userId, String password, String name) {
+        return new User(userId, password, name);
+    }
 
-        public Builder(String userId, String password) {
-            this.userId = userId;
-            this.password = password;
-        }
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public User build() {
-            return new User(userId, password, name);
-        }
+    public static User createUserForLogin(String userId, String password) {
+        return new User(userId, password, "");
     }
 
     public String getUserId() {
         return userId;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean hasPassword(final String password) {
