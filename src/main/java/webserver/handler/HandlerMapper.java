a085b959f757c19ff3ce43d13215bf7c93306b22
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-03-28 11:18:22 +0900

    refactor: mapper에 OptionalBody넣는 방식 변경

--- src/main/java/webserver/handler/HandlerMapper.java
+++ src/main/java/webserver/handler/HandlerMapper.java
@@ -2,7 +2,6 @@ package webserver.handler;
 
 import http.requestMessage.Request;
 import http.requestMessage.Uri;
-import http.responseMessage.Response;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +13,10 @@ public class HandlerMapper {
 
     public HandlerMapper(final Request request) {
         this.uri = request.getUri();
-        handlerMapper.put(uri -> request.isPost() && uri.isSame("/login"), new LoginHandler(request.getOptBody().get()));
-        handlerMapper.put(uri -> request.isPost() && uri.isSame("/create"), new RegisterHandler(request.getOptBody().get()));
+        request.getOptBody().ifPresent(body -> {
+            handlerMapper.put(uri -> request.isPost() && uri.isSame("/login"), new LoginHandler(body));
+            handlerMapper.put(uri -> request.isPost() && uri.isSame("/create"), new RegisterHandler(body));
+        });
         handlerMapper.put(uri -> request.isPost() && uri.isSame("/logout"), new LogOutHandler(request.getHeader()));
         handlerMapper.put(uri -> request.isGet(), new GetHandler(uri));
     }
@@ -26,7 +27,5 @@ public class HandlerMapper {
                 .map(handlerMapper::get)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("404에러"));
-
-        return handler.handle();
     }
 }
