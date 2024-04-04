8bd8a33cc8f85097c74b4c0c4b654632aa8d41d2
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-04-04 17:21:32 +0900

    refactor: 패스워드로 유저를 비교하지 않고 유저끼리 내부에서 비교

---
+++ src/main/java/webserver/authentication/UserList.java
@@ -0,0 +1,49 @@
+package webserver.authentication;
+
+import http.SessionDB;
+import http.responseMessage.Response;
+import http.responseMessage.StatusLine;
+import webserver.db.Database;
+import webserver.model.User;
+
+import static java.util.stream.Collectors.joining;
+
+public class UserList implements UserAuthentication {
+    private final String sessionID;
+
+    public UserList(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    @Override
+    public boolean isPass() {
+        return SessionDB.hasSessionID(sessionID);
+    }
+
+    @Override
+    public void handleSessionDB() {
+
+    }
+
+    @Override
+    public Response getSuccessResponse() {
+        final String allUserNames = Database.findAllUser().stream()
+                .map(User::getName)
+                .collect(joining("\n"));
+
+        byte[] data = allUserNames.getBytes();
+        String subType = "text/plain";
+
+        return new Response.Builder(StatusLine.OK_200)
+                .contentType(subType)
+                .contentLength(data)
+                .build();
+    }
+
+    @Override
+    public Response getFailedResponse() {
+        return new Response.Builder(StatusLine.Found_302)
+                .location("/login/index.html")
+                .build();
+    }
+}
