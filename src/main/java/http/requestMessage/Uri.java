2de288e8b86b10f8b1ee9db6eded329434c3c4e5
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-04-04 17:20:36 +0900

    chore: 사용하지 않는 메서드 및 클래스 제거

--- src/main/java/http/requestMessage/Uri.java
+++ src/main/java/http/requestMessage/Uri.java
@@ -3,11 +3,8 @@ package http.requestMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-
 public class Uri {
     private static final Logger logger = LoggerFactory.getLogger(Uri.class);
-    private static final String DIRECTORY = "src/main/resources/static";
 
     private final String uri;
 
@@ -16,12 +13,7 @@ public class Uri {
         this.uri = uri;
     }
 
-    public boolean corresponds(String path) {
-        return uri.equals(path);
-    }
-
-    boolean hasResource() {
-        File file = new File(DIRECTORY + uri);
-        return file.exists();
+    public String getUri() {
+        return uri;
     }
 }
